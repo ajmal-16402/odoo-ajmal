a099816cd4e217ac8025cdf4aeaffbcaae23b6a5
ajmal <ajmal@loyalitsolutions.com>
2024-08-01 13:58:32 +0530

    update for new file

---
+++ zinfog_task/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Zinfog Task",
+
+    'summary': """
+       Confirm a sale order and create multiple deliveries against each product.
+        Quotation should contain a mix of different products
+        Identical products must include in a single delivery.""",
+
+    'description': """
+        Confirm a sale order and create multiple deliveries against each product.
+        Quotation should contain a mix of different products
+        Identical products must include in a single delivery.
+    """,
+
+    'author': "Ajmal ",
+    'category': 'sales',
+    'version': '16.0.1',
+    'license': 'LGPL-3',
+
+    'depends': ['base','stock','sale_management'],
+
+    'data': [
+    ],
+
+}
---
+++ zinfog_task/models/saleorder_inherit.py
@@ -0,0 +1,45 @@
+from odoo import models, api
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        print("res=",res)
+        for order in self:
+            product_lines = {}
+            for line in order.order_line:
+                product = line.product_id
+                print("product=",product)
+                if product not in product_lines:
+                    product_lines[product] = self.env['sale.order.line']
+                    print("product_lines=",product_lines)
+                product_lines[product] |= line
+
+            for product, lines in product_lines.items():
+                picking_type_id = self.env['stock.picking.type'].search([("code", "=", 'outgoing')], limit=1)
+                picking = self.env['stock.picking'].create({
+                    'partner_id': order.partner_id.id,
+                    'picking_type_id': picking_type_id.id,
+                    'origin': order.name,
+                    'location_id': order.warehouse_id.lot_stock_id.id,
+                    'location_dest_id': order.partner_id.property_stock_customer.id,
+                    'sale_id': order.id,
+                })
+                for line in lines:
+                    print("oooooo")
+                    k=self.env['stock.move'].create({
+                        'name': line.name,
+                        'product_id': line.product_id.id,
+                        'product_uom_qty': line.product_uom_qty,
+                        'product_uom': line.product_uom.id,
+                        'picking_id': picking.id,
+                        'location_id': order.warehouse_id.lot_stock_id.id,
+                        'location_dest_id': order.partner_id.property_stock_customer.id,
+                        'sale_line_id': line.id,
+                    })
+                    print("k========",k)
+                picking.action_confirm()
+                picking.action_assign()
+
+        return res
